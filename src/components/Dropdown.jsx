d8c3bc3f37981853ca0c3bf5e2f14e171331001b
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-01-27 23:39:38 +0000

    update Dropdown to return <select> menu with options that submit coordinates onChange

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,30 +1,52 @@
 
-import { getImageDimensions, normaliseCoordinates, getFileDimensions, submitCoordinates } from '../../lib/utils/helpers.js'
+import { useState } from 'react'
+import { getImageDimensions, normaliseCoordinates, getFileDimensions } from '../../lib/utils/helpers.js'
+import { submitCoordinates } from '../../lib/utils/api_helpers.js'
 
 function Dropdown({ isClicked, image, imageEvent }) {
 
-   const handleClick = (e) => {
+   const [ selectedCharacter, setSelectedCharacter ] = useState()
 
-      // some kind of detection for if user clicks away from Dropbox
-      console.log(e)
+   function handleChange(e) {
+      // DROPDOWN INFO
+      const options = [...e.target.selectedOptions]
+      const values = options.map(option => option.value)
 
+      setSelectedCharacter(values)
+      // some kind of detection for if user clicks away from Dropbox... console.log(e)
+      // IMAGE INFO
       const { fileHeight, fileWidth } = getFileDimensions(imageEvent) 
       const elementDimensions = getImageDimensions(image)
       const coordinates = normaliseCoordinates(imageEvent, elementDimensions, fileHeight, fileWidth)
-      submitCoordinates(coordinates)
+   
+      submitCoordinates(values[0], coordinates)     
    }
 
+   // maybe an else to remove element?
    if (isClicked) {
       const { clientX, clientY } = imageEvent.current
       console.log(clientX, clientY)
    
       const styles = {
          left: clientX,
-         top: clientY
+         top: clientY,
       }
       
       return (
-         <h1 style={styles} className="clicked" onClick={(e) => handleClick(e)}>CLICKED!</h1>
+         <div style={styles} className='clicked'>
+            <form action="#">
+               <label htmlFor="character">Character: </label>
+               <select
+                  onChange={(e) => handleChange(e)}
+                  value={selectedCharacter} 
+                  name="characters" 
+                  id="character">
+               <option value="char1">char1</option>
+               <option value="char2">char2</option>
+               <option value="char3">char3</option>
+               </select>
+            </form>
+         </div>
       )
    }
 }
