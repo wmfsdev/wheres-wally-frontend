399b5ae8195e5c1a4ff9e51bc4081c0b2184daa7
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-02-01 23:51:13 +0000

    replace onChange prop with onClick and update option value prop

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -5,21 +5,26 @@ import { submitCoordinates } from '../../lib/utils/api_helpers.js'
 
 function Dropdown({ isClicked, image, imageEvent }) {
 
-   const [ selectedCharacter, setSelectedCharacter ] = useState()
+   const [ selectedCharacter, setSelectedCharacter ] = useState('')
 
-   function handleChange(e) {
+   function handleClick(e) {
       // DROPDOWN INFO
+      console.log(selectedCharacter)
       const options = [...e.target.selectedOptions]
       const values = options.map(option => option.value)
 
-      setSelectedCharacter(values)
+      setSelectedCharacter(values[0])
       // some kind of detection for if user clicks away from Dropbox... console.log(e)
       // IMAGE INFO
       const { fileHeight, fileWidth } = getFileDimensions(imageEvent) 
       const elementDimensions = getImageDimensions(image)
       const coordinates = normaliseCoordinates(imageEvent, elementDimensions, fileHeight, fileWidth)
    
-      submitCoordinates(values[0], coordinates)     
+      submitCoordinates(values[0], coordinates)  
+      
+      // function to handle server response
+      //    if character found  find way to not be able to resubmit
+      //    
    }
 
    // maybe an else to remove element?
@@ -37,13 +42,12 @@ function Dropdown({ isClicked, image, imageEvent }) {
             <form action="#">
                <label htmlFor="character">Character: </label>
                <select
-                  onChange={(e) => handleChange(e)}
-                  value={selectedCharacter} 
+                  onClick={(e) => handleClick(e)}
                   name="characters" 
                   id="character">
-               <option value="char1">char1</option>
-               <option value="char2">char2</option>
-               <option value="char3">char3</option>
+               <option value="1">char1</option>
+               <option value="2">char2</option>
+               <option value="3">char3</option>
                </select>
             </form>
          </div>
