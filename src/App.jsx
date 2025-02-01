42feb0b760fdf748436bbb9a242be581df84d342
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-02-01 23:57:33 +0000

    import and create useEffect hook to make fetch request to backend on page render

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 
 import './App.css'
-
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { Dropdown } from './components/Dropdown'
 
 function App() {
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log("fetching")
+      await fetch('http://localhost:3000/game/board', {
+        credentials: 'include'
+      })
+    }
+    fetchData()
+  }, [])
 
   const imageRef = useRef(null)
   let imageClickEvent = useRef(null)
   const [ clickState, setClickState ] = useState(false)
 
+  // some other clickState required, switch to false when user clicks away from Dropbox
   
   const handleImageClick = (e) => {
     setClickState(true)
-    console.log(e)
     imageClickEvent.current = e
   }
 
@@ -21,7 +30,7 @@ function App() {
     <>
       <Dropdown isClicked={clickState} image={imageRef} imageEvent={imageClickEvent}/>
       <div className="wally-border">
-        <img ref={imageRef} onClick={(e) => handleImageClick(e, imageClickEvent)} src="/src/assets/r_wally.jpg" alt="wheres wally" />
+        <img ref={imageRef} onClick={(e) => handleImageClick(e)} src="/src/assets/r_wally.jpg" alt="wheres wally" />
       </div>
     </>
   )
