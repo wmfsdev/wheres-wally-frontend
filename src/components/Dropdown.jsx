7149b946d9c4fe758b6be2fb35ecc4347d41b10e
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-02-10 22:15:57 +0000

    switch hardcoded (html) options for array stored in state, passed to gameProgress function including 'characters' component prop

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,41 +1,47 @@
 
 import { useState } from 'react'
-import { getImageDimensions, normaliseCoordinates, getFileDimensions } from '../../lib/utils/helpers.js'
+import { getImageDimensions, normaliseCoordinates, getFileDimensions, gameProgress } from '../../lib/utils/helpers.js'
 import { submitCoordinates } from '../../lib/utils/api_helpers.js'
 
-function Dropdown({ handleOption, isClicked, image, imageEvent }) {
+function Dropdown({ characters, handleOption, isClicked, image, imageEvent }) {
 
    const [ selectedCharacter, setSelectedCharacter ] = useState('')
+   const [ selectOptions, setSelectOptions ] = useState([
+      { id: 1, value: "defaultchar", name: "DEFAULT" },
+      { id: 2, value: "2ndchar", name: "2NDCHAR" } 
+   ])
 
-   function handleClick(e) {
+   async function handleClick(e) {
       // DROPDOWN INFO
-      console.log(selectedCharacter)
       const options = [...e.target.selectedOptions]
       const values = options.map(option => option.value)
-
+      const names = options.map(option => option.text)
+      
       setSelectedCharacter(values[0])
       // IMAGE INFO
       const { fileHeight, fileWidth } = getFileDimensions(imageEvent) 
       const elementDimensions = getImageDimensions(image)
       const coordinates = normaliseCoordinates(imageEvent, elementDimensions, fileHeight, fileWidth)
+      const { status } = await submitCoordinates(values[0], coordinates)
    
-      submitCoordinates(values[0], coordinates)  
-      handleOption()
-      // function to handle server response
-      //    if character found  find way to not be able to resubmit
-      //    
+      handleOption() 
+      characters({ name: names[0] })
+      gameProgress(status, setSelectOptions, selectOptions, values[0])
    }
 
+   const options = selectOptions.map((option) => 
+      <option key={option.id} value={option.value}>{option.name}</option>
+   )
+
    if (isClicked) {
-      const { clientX, clientY } = imageEvent.current
-      console.log(clientX, clientY)
-   
+      const { clientX, clientY } = imageEvent.current   
       const styles = {
          left: clientX,
          top: clientY,
       }
       
       return (
+         <>
          <div style={styles} className='clicked'>
             <form action="#">
                <label htmlFor="character">Character: </label>
@@ -43,12 +49,11 @@ function Dropdown({ handleOption, isClicked, image, imageEvent }) {
                   onClick={(e) => handleClick(e)}
                   name="characters" 
                   id="character">
-               <option value="defaultchar">char1</option>
-               <option value="2">char2</option>
-               <option value="3">char3</option>
+                  {options}
                </select>
             </form>
          </div>
+         </>
       )
    }
 }
