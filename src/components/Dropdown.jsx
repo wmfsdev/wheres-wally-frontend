e9d94e32ad629460ad9f9856cd2762439b0b6768
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-02-20 20:03:12 +0000

    import useParams for passing url parameter to gameProgress function

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,10 +2,13 @@
 import { useState } from 'react'
 import { getImageDimensions, normaliseCoordinates, getFileDimensions, gameProgress } from '../../lib/utils/helpers.js'
 import { submitCoordinates } from '../../lib/utils/api_helpers.js'
+import { useParams } from 'react-router'
+
 
 function Dropdown({ submit, characters, handleOption, isClicked, image, imageEvent }) {
 
-   const [ selectedCharacter, setSelectedCharacter ] = useState('')
+   const { id } = useParams()
+   // const [ selectedCharacter, setSelectedCharacter ] = useState('')
    const [ selectOptions, setSelectOptions ] = useState([
       { id: 1, value: "defaultchar", name: "DEFAULT" },
       { id: 2, value: "2ndchar", name: "2NDCHAR" } 
@@ -17,15 +20,16 @@ function Dropdown({ submit, characters, handleOption, isClicked, image, imageEve
       const values = options.map(option => option.value)
       const names = options.map(option => option.text)
       
-      setSelectedCharacter(values[0])
+     // setSelectedCharacter(values[0])
       // IMAGE INFO
       const { fileHeight, fileWidth } = getFileDimensions(imageEvent) 
       const elementDimensions = getImageDimensions(image)
       const coordinates = normaliseCoordinates(imageEvent, elementDimensions, fileHeight, fileWidth)
+
       const response = await submitCoordinates(values[0], coordinates)
       
       handleOption() // handles appearance of dropdown menu 
-      await gameProgress(response, setSelectOptions, selectOptions, values[0])
+      await gameProgress(id, response, setSelectOptions, selectOptions, values[0])
       
       if (response.gameRuntime) {
          console.log("win with time")
@@ -39,10 +43,11 @@ function Dropdown({ submit, characters, handleOption, isClicked, image, imageEve
    )
 
    if (isClicked) {
-      const { clientX, clientY } = imageEvent.current   
+      const { pageX, pageY } = imageEvent.current
+      console.log("isClicked: ", pageY)
       const styles = {
-         left: clientX,
-         top: clientY,
+         left: pageX,
+         top: pageY,
       }
       
       return (
