8bccb942f9394b9f2b6b41f09e3596ef770cd6fa
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-02-22 19:43:56 +0000

    create visibility toggle for list of playable boards

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
 
 import './App.css'
 import { Link, Outlet } from "react-router-dom"
+import { useState } from 'react'
 
 function App() {
 
+  const [ hidden, setHidden ] = useState(true)
+
+  const list = <ol className="board-list">
+  <li>
+    <Link to='/board/maze'>
+      <img src="/src/assets/wally_maze_thumb.jpeg" alt="" />
+    </Link>
+  </li>
+  <li>
+    <Link to='/board/football'>
+      <img src="/src/assets/wally_football_thumb.jpeg" alt="" />
+    </Link>
+  </li>
+  <li>
+    <Link to='/board/fishing'>
+      <img src="/src/assets/wally_fishing_thumb.jpeg" alt="" />
+    </Link>
+  </li>
+  </ol>
+
   return (
     <>
-    <nav>
-    <h1>PICK A BOARD</h1>
-    <ol className="board-list">
-      <li>
-        <Link to='/board/maze'>
-          <img src="/src/assets/wally_maze_thumb.jpeg" alt="" />
-        </Link>
-      </li>
-      <li>
-        <Link to='/board/football'>
-          <img src="/src/assets/wally_football_thumb.jpeg" alt="" />
-        </Link>
-      </li>
-      <li>
-        <Link to='/board/fishing'>
-          <img src="/src/assets/wally_fishing_thumb.jpeg" alt="" />
-        </Link>
-      </li>
-    </ol>
+    <nav
+      onMouseEnter={() => setHidden(false)}
+      onMouseLeave={() => setHidden(true)}   
+      id='app-nav'>
+      <h1>PICK A BOARD</h1>
+      { hidden ? null : list } 
     </nav>
     <Outlet />
     </>
