5a63f006bef45b90dbb797749bc7b4a7826b5eaa
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-02-22 20:08:13 +0000

    update state for selectOptions with new characters and pass in new arguments to gameProgress function for correctly rendering outcome of attempt at identifying character

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,10 +8,10 @@ import { useParams } from 'react-router'
 function Dropdown({ submit, characters, handleOption, isClicked, image, imageEvent }) {
 
    const { id } = useParams()
-   // const [ selectedCharacter, setSelectedCharacter ] = useState('')
    const [ selectOptions, setSelectOptions ] = useState([
-      { id: 1, value: "defaultchar", name: "DEFAULT" },
-      { id: 2, value: "2ndchar", name: "2NDCHAR" } 
+      { id: 1, value: "wilma", name: "WILMA" },
+      { id: 2, value: "wally", name: "WALLY" },
+      { id: 3, value: "wizard", name: "WIZARD" }
    ])
 
    async function handleClick(e) {
@@ -19,23 +19,21 @@ function Dropdown({ submit, characters, handleOption, isClicked, image, imageEve
       const options = [...e.target.selectedOptions]
       const values = options.map(option => option.value)
       const names = options.map(option => option.text)
-      
-     // setSelectedCharacter(values[0])
       // IMAGE INFO
       const { fileHeight, fileWidth } = getFileDimensions(imageEvent) 
       const elementDimensions = getImageDimensions(image)
       const coordinates = normaliseCoordinates(imageEvent, elementDimensions, fileHeight, fileWidth)
 
-      const response = await submitCoordinates(values[0], coordinates)
+      const response = await submitCoordinates(values[0], coordinates, id)
       
       handleOption() // handles appearance of dropdown menu 
-      await gameProgress(id, response, setSelectOptions, selectOptions, values[0])
+      await gameProgress(id, response, setSelectOptions, selectOptions, values[0], characters, names[0])
       
       if (response.gameRuntime) {
          console.log("win with time")
          submit(response.playerId)
       }
-      characters({ name: names[0] })
+    //  characters({ name: names[0] })
    }
 
    const options = selectOptions.map((option) => 
@@ -44,7 +42,10 @@ function Dropdown({ submit, characters, handleOption, isClicked, image, imageEve
 
    if (isClicked) {
       const { pageX, pageY } = imageEvent.current
-      console.log("isClicked: ", pageY)
+      const offset = imageEvent.current.target.offsetTop
+      console.log("X: ", pageX)
+      console.log("Y: ", pageY - offset)
+
       const styles = {
          left: pageX,
          top: pageY,
