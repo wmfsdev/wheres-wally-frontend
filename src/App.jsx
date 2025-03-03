795de8f7772c58f87776d20f2b0c5593c179e0a3
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-03 20:25:46 +0000

    import Characters component, include with Outlet in conditional render, and pass state accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@
 import './App.css'
 import { Link, Outlet } from "react-router-dom"
 import { useState } from 'react'
+import Characters from './components/Characters'
+
+import { useParams } from 'react-router-dom'
 
 function App() {
 
+  const { id } = useParams()
+  const [ param, setParam ] = useState(id)
   const [ hidden, setHidden ] = useState(true)
+  const [ boardState, setBoardState ] = useState(false)
+  const [ characters, setCharacters ] = useState([])
+
+  const [ submitState, setSubmitState ] = useState(null)
 
   const list = <ol className="board-list">
   <li>
@@ -23,18 +32,28 @@ function App() {
       <img src="/src/assets/wally_fishing_thumb.jpeg" alt="" />
     </Link>
   </li>
+  
   </ol>
 
   return (
     <>
-    <nav
-      onMouseEnter={() => setHidden(false)}
-      onMouseLeave={() => setHidden(true)}   
-      id='app-nav'>
-      <h1>PICK A BOARD</h1>
-      { hidden ? null : list } 
-    </nav>
-    <Outlet />
+    <div className='nav-wrapper'>
+      <nav
+        onMouseEnter={() => setHidden(false)}
+        onMouseLeave={() => setHidden(true)}   
+        className='board-nav'>
+        <h2><Link to={'/results'}>WHO IS THE FASTEST?</Link></h2>
+        <h2>PICK A BOARD</h2>
+        { hidden ? null : list } 
+      </nav>
+      { boardState ? 
+        <div className='character-nav'>
+          <h2>FIND THESE CHARACTERS:</h2>
+          <Characters charArray={characters} param={param} submitState={submitState}/>
+        </div>
+        : null }
+    </div>
+    <Outlet context={[setBoardState, characters, setCharacters, param, setParam, submitState, setSubmitState ]}/>
     </>
     )
 }
