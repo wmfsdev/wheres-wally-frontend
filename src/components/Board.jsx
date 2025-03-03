c20e55d4f5463bba9495798592a1274c976b424d
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-03 20:31:26 +0000

    remove submitPlayerName function, add useOutletContext and reorganise state

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,22 @@
 
 import { useRef, useState, useEffect } from 'react'
 import { Dropdown } from './Dropdown'
-import { useNavigate } from 'react-router'
 import { useParams } from 'react-router'
+import { useOutletContext } from 'react-router'
 
 function Board() {
 
   const { id } = useParams()
+  const [ setBoardState, characters, setCharacters, param, setParam, submitState, setSubmitState] = useOutletContext()
+  
+  useEffect(() => {
+    setParam(id)
+  })
 
+  useEffect(() => {
+    setBoardState(true)
+  },)
+  
   useEffect(() => {
     async function fetchData() {
       console.log("fetching")
@@ -16,54 +25,18 @@ function Board() {
       })
     }
     fetchData()
-  }, [])
+  }, [param])
   
-  const navigate = useNavigate()
   const imageRef = useRef(null)
   let imageClickEvent = useRef(null)
-
-  const [ submitState, setSubmitState ] = useState(null)
-
   const [ clickState, setClickState ] = useState(false)
-  const [ characters, setCharacters ] = useState([])
 
   const handleImageClick = (e) => {
-    console.log("being clicked")
-
     if (!clickState) {
       imageClickEvent.current = e 
       setClickState(true)
     } else {
       setClickState(false)
-    }
-  }
-
-  const options = characters.map(function(option, index) { 
-    return <div className='character' key={index}>{option.name}</div>
-  })
-
-  async function submitPlayerName(e) {
-    e.preventDefault()
-    console.log("submitting")
-    const form = e.target
-    const formData = new FormData(form)
-    const playerId = formData.get('playerId')
-    const playerName = formData.get('playerName')
- 
-    try {
-      const response = await fetch('http://localhost:3000/game/player', {
-          method: 'PUT',
-          headers: { 'Content-type': 'application/json; charset=UTF-8',
-          },
-          body: JSON.stringify({
-             playerId: playerId,
-             playerName: playerName,
-          })
-      })
-      const data = await response.json()
-      navigate('/results')
-    } catch (err) {
-      console.log(err)
     }
   }
 
@@ -83,16 +56,6 @@ function Board() {
         <img ref={imageRef} onClick={(e) => handleImageClick(e)} src={`/src/assets/wally_${id}.jpeg`} alt="wheres wally" />
       </div>
       <div className='characters'>
-        <h2>You have found these characters:</h2>
-        {options}
-        { submitState ?
-        <>
-          <form action="PUT" onSubmit={submitPlayerName}>
-            <input type="hidden" name="playerId" value={submitState} readOnly={true}/>
-            <input type="text" name="playerName" />
-            <button type="submit">submit</button>
-          </form>
-        </> : null  }
       </div>
     </div>
   )
