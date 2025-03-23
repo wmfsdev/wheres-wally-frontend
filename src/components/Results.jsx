f3faa5d5d048f4991275545875e262a34ae5123f
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-23 22:36:01 +0000

    add destructured values to useLoaderData call and reorganise component's HTML structure

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,26 +4,42 @@ import { Link } from "react-router-dom"
 
 function Results() {
 
-   const results = useLoaderData()
-   console.log(results)
+   const { maze, football, rome } = useLoaderData()
 
    return (
       <>
       <h1>LEADER BOARD</h1>
-      <ul className="result-titles">
-         <li><p>Name</p></li>
-         <li><p>Game Length</p></li>
-         <li><p>Board</p></li>
-      </ul>
-      <ul className="result-list">
-         { results.map((result, index) =>
-         <li className="result-list-item" key={index}>
-            <p>{result.name}</p>
-            <p>{result.gameLength} seconds</p>
-            <p>{result.board}</p>
-         </li>)
-         }
-      </ul>
+      <div className="results-wrapper">
+         <div className="result-list-wrapper">
+            <ul className="result-list">
+               <h2>MAZE</h2>
+               { maze.map((result, index) =>
+               <li className="result-list-item" key={index}>
+                  <p>{result.name}</p>
+                  <p>{result.gameLength} seconds</p>          
+               </li>)
+               }
+            </ul>
+            <ul className="result-list">
+               <h2>FOOTBALL</h2>
+               { football.map((result, index) =>
+               <li className="result-list-item" key={index}>
+                  <p>{result.name}</p>
+                  <p>{result.gameLength} seconds</p>
+               </li>)
+               }
+            </ul>
+            <ul className="result-list">
+               <h2>ROME</h2>
+               { rome.map((result, index) =>
+               <li className="result-list-item" key={index}>
+                  <p>{result.name}</p>
+                  <p>{result.gameLength} seconds</p>
+               </li>)
+               }
+            </ul>
+         </div>
+      </div>
       <h2 className="play-again"><Link to={'/'}>Play Again?</Link></h2>
       </>
    )
