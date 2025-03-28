90b755a03ffbd2af7c60d485c9b3732b5c0d065f
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-03-28 22:37:27 +0000

    add Link elements to result lists

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,12 @@
 
-import { useLoaderData } from "react-router"
+import { useLoaderData, useLocation } from "react-router"
 import { Link } from "react-router-dom"
 
 function Results() {
 
    const { maze, football, rome } = useLoaderData()
+   const { state } = useLocation();
+   console.log(state)
 
    return (
       <>
@@ -12,30 +14,36 @@ function Results() {
       <div className="results-wrapper">
          <div className="result-list-wrapper">
             <ul className="result-list">
-               <h2>MAZE</h2>
-               { maze.map((result, index) =>
-               <li className="result-list-item" key={index}>
-                  <p>{result.name}</p>
-                  <p>{result.gameLength} seconds</p>          
-               </li>)
+               <h2><Link to='/board/maze'>MAZE</Link></h2>
+               { maze.length === 0 ?
+                  <p>No one has complete this yet!</p> : 
+                  maze.map((result, index) =>
+                     <li className="result-list-item" key={index}>
+                     <p>{result.name}</p>
+                     <p>{result.gameLength} seconds</p>          
+                     </li>)
                }
             </ul>
             <ul className="result-list">
-               <h2>FOOTBALL</h2>
-               { football.map((result, index) =>
-               <li className="result-list-item" key={index}>
-                  <p>{result.name}</p>
-                  <p>{result.gameLength} seconds</p>
-               </li>)
+               <h2><Link to='/board/football'>FOOTBALL</Link></h2>
+               { football.length === 0 ?
+                  <p>No entries... yet!</p> : 
+                  football.map((result, index) =>
+                     <li className="result-list-item" key={index}>
+                     <p>{result.name}</p>
+                     <p>{result.gameLength} seconds</p>          
+                     </li>)
                }
             </ul>
             <ul className="result-list">
-               <h2>ROME</h2>
-               { rome.map((result, index) =>
-               <li className="result-list-item" key={index}>
-                  <p>{result.name}</p>
-                  <p>{result.gameLength} seconds</p>
-               </li>)
+               <h2><Link to='/board/rome'>ROME</Link></h2>
+               { rome.length === 0 ?
+                  <p>No entries... yet!</p> : 
+                  rome.map((result, index) =>
+                     <li className="result-list-item" key={index}>
+                     <p>{result.name}</p>
+                     <p>{result.gameLength} seconds</p>          
+                     </li>)
                }
             </ul>
          </div>
