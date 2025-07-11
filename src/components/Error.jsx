2a78574ee5c34d3444caf5918be9bf9246c8fbef
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-07-11 20:13:19 +0100

    update Error message with new information regarding cookies

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom"
 
 function Error() {
-
-    return (
-        <div className="error-headers">
-            <h1>Something went wrong...</h1>
-            <h2><Link to='/'>Try again?</Link></h2>
-        </div>
-    )
+	return (
+		<div className="error-headers">
+			<h1>Something went wrong...</h1>
+			<h2>You may have third party cookies blocked. You can disable this feature in your browser settings and...</h2>
+			<h2><Link to='/'>Try again?</Link></h2>
+		</div>
+	)
 }
 
 export default Error
